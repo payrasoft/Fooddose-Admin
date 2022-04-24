07bcfa781191c5930275b31b4b58d43972e40cd4
raihan50017 <aburaihan50017@gmail.com>
2022-04-24 18:51:53 +0600

    24/4/2022-update

--- src/Components/Admin/Merchants/AddMerchants.js
+++ src/Components/Admin/Merchants/AddMerchants.js
@@ -1,47 +1,147 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../Header";
 
 const AddMerchants = (props) => {
+  const form = useRef(null);
 
-  const form = useRef(null)
+  const [name, setName] = useState("");
+  const [shopName, setShopName] = useState("");
+  const [shopLogo, setShopLogo] = useState(null);
+  const [shopAddress, setShopAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [website, setWebsite] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [status, setStatus] = useState("true");
 
-  const submit = e => {
-    e.preventDefault()
-    const data = new FormData(form.current)
-    fetch('http://127.0.0.1:8000/api/merchant', { method: 'POST', body: data})
-      .then(res => res.json())
-      .then(json => console.log(json.merchant))
-  }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    if (name === "name") {
+      setName(event.target.value);
+    }
+    if (name === "shopName") {
+      setShopName(event.target.value);
+    }
+    if (name === "shopAddress") {
+      setShopAddress(event.target.value);
+    }
+    if (name === "email") {
+      setEmail(event.target.value);
+    }
+    if (name === "phone") {
+      setPhone(event.target.value);
+    }
+    if (name === "website") {
+      setWebsite(event.target.value);
+    }
+    if (name === "password") {
+      setPassword(event.target.value);
+    }
+    if (name === "confirmPassword") {
+      setConfirmPassword(event.target.value);
+    }
+  };
+
+  const handleFileInput = (event) => {
+    setShopLogo(event.target.files[0]);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    const data = new FormData(form.current);
+
+    data.append("name", name);
+    data.append("shopName", shopName);
+    data.append("shopLogo", shopLogo);
+    data.append("shopAddress", shopAddress);
+    data.append("email", email);
+    data.append("phone", phone);
+    data.append("website", website);
+    data.append("password", password);
+    data.append("confirmPassword", confirmPassword);
+    data.append("marchent", status);
+
+    console.log(data.get("shopLogo"));
+
+    fetch("http://127.0.0.1:8000/api/merchant", { method: "POST", body: data })
+      .then((res) => res.json())
+      .then((json) => console.log(json.merchant));
+  };
 
   return (
     <div>
       <Header />
       <main className="mt-5 pt-5">
         <div className="container-fluid">
-          <div className="d-flex justify-content-between top-content">
-            <h4>Add Merchants</h4>{" "}
-            <button className="btn btn-success mb-2">
+          <div
+            style={{ backgroundColor: "rgb(255, 90, 0, .6)" }}
+            className="d-flex justify-content-between top-content align-items-center p-2"
+          >
+            <h3 style={{ color: "#640000" }}>Add Merchants</h3>{" "}
+            <button className="btn btn-success">
               <Link to="/all-merchants">All Merchant</Link>
             </button>
           </div>
           <div className="card-body border rounded">
             <form ref={form} onSubmit={submit} className="add-merchant">
-              <h5 className="text-success mt-2">Merchant Information</h5>
+              <h4 className="text-success mt-2 mb-3">
+                Merchant <span style={{ color: "#640000" }}>Information</span>
+              </h4>
               <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="m-name">Merchant Name</label>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="mName">Merchant Name</label>
                   <input
+                    onChange={handleChange}
                     className="form-control"
                     type="text"
                     name="name"
-                    id="m-name"
+                    id="mName"
+                    value={name}
                     placeholder="Merchant Name"
                   />
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="shopName">Shop Name</label>
+                  <input
+                    onChange={handleChange}
+                    value={shopName}
+                    className="form-control"
+                    type="text"
+                    name="shopName"
+                    id="shopName"
+                    placeholder="shop Name"
+                  />
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="shopLogo">Shop Logo</label>
+                  <input
+                    onChange={handleFileInput}
+                    className="form-control"
+                    type="file"
+                    name="shopLogo"
+                    id="shopLogo"
+                    placeholder="Shop logo"
+                  />
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="shopAddress">Shop Address</label>
+                  <input
+                    onChange={handleChange}
+                    value={shopAddress}
+                    className="form-control"
+                    type="text"
+                    name="shopAddress"
+                    id="shopAddress"
+                    placeholder="Shop address"
+                  />
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
                   <label htmlFor="m-email">Email</label>
                   <input
+                    onChange={handleChange}
+                    value={email}
                     className="form-control"
                     type="email"
                     name="email"
@@ -49,9 +149,11 @@ const AddMerchants = (props) => {
                     placeholder="Email"
                   />
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
                   <label htmlFor="mobile">Mobile</label>
                   <input
+                    onChange={handleChange}
+                    value={phone}
                     className="form-control"
                     type="text"
                     name="phone"
@@ -59,29 +161,23 @@ const AddMerchants = (props) => {
                     placeholder="Mobile"
                   />
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="address">Address</label>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="website">Website</label>
                   <input
+                    onChange={handleChange}
+                    value={website}
                     className="form-control"
                     type="text"
-                    name="address"
-                    id="address"
-                    placeholder="Address"
+                    name="website"
+                    id="website"
+                    placeholder="Website"
                   />
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="status">Status</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="status"
-                    id="status"
-                    placeholder="Status"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
                   <label htmlFor="password">Password</label>
                   <input
+                    onChange={handleChange}
+                    value={password}
                     className="form-control"
                     type="password"
                     name="password"
@@ -89,211 +185,30 @@ const AddMerchants = (props) => {
                     placeholder="Password"
                   />
                 </div>
-              </div>
-              <h5 className="text-success mt-3">
-                Merchant Additional Information
-              </h5>
-              <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="b-name">Business Name</label>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  <label htmlFor="confirmPassword">Confirm Password</label>
                   <input
+                    onChange={handleChange}
+                    value={confirmPassword}
                     className="form-control"
-                    type="text"
-                    name="business_name"
-                    id="b-name"
-                    placeholder="Business Name"
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    placeholder="Confirm Password"
                   />
                 </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="bkash">Bkash Number</label>
+                <div className="col-lg-6 col-md-6 col-sm-6 mb-3">
+                  {/* <label htmlFor="status">Status</label> */}
                   <input
-                    className="form-control"
-                    type="number"
-                    name="bkash_number"
-                    id="bkash"
-                    placeholder="Bkash Number"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="rocket">Rocet Number</label>
-                  <input
+                    hidden
+                    value={"true"}
                     className="form-control"
                     type="text"
-                    name="rocket_number"
-                    id="rocket"
-                    placeholder="Rocet Number"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="nagad">Nagad Number</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="nagad_number"
-                    id="nagad"
-                    placeholder="Nagad Number"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="bank-name">Bank Name</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="bank_name"
-                    id="bank-name"
-                    placeholder="Bank Name"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="a-number">Account Number</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="account_number"
-                    id="a-number"
-                    placeholder="Account Number"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="a-name">Account Name</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="account_name"
-                    id="a-name"
-                    placeholder="Account Name"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="p-method">Payment Method</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="payment_method"
-                    id="p-method"
-                    placeholder="Payment Method"
-                  />
-                </div>
-              </div>
-
-              <h5 className="text-success mt-3">Restaurant Information</h5>
-              <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="r-name">Restaurant Name</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[name]"
-                    id="r-name"
-                    placeholder="Restaurant Name"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="r-code">Restaurant Code</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[code]"
-                    id="r-code"
-                    placeholder="Restaurant Code"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="r-address">Restaurant Address</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[address]"
-                    id="r-address"
-                    placeholder="Restaurant Address"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="t-number">Tin Number</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[tin]"
-                    id="t-number"
-                    placeholder="Tin Number"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="since">Since</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[since]"
-                    id="since"
-                    placeholder="Since"
-                  />
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-6">
-                  <label htmlFor="r-status">Status</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="restaurant[status]"
-                    id="r-status"
+                    name="status"
+                    id="status"
                     placeholder="Status"
                   />
                 </div>
-              </div>
-
-              <h5 className="text-success mt-3">Schedule Information</h5>
-              <div className="days d-flex justify-content-between">
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Saturday"  value="Saturday" />
-                  <label className="" htmlFor="Saturday">
-                    Saturday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Sunday"  value="Sunday" />
-                  <label className="" htmlFor="Sunday">
-                    Sunday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Monday"  value="Monday" />
-                  <label className="" htmlFor="Monday">
-                    Monday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Tuesday"  value="Tuesday" />
-                  <label className="" htmlFor="Tuesday">
-                    Tuesday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Wednesday"  value="Wednesday" />
-                  <label className="" htmlFor="Wednesday">
-                    Wednesday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Thursday"  value="Thursday" />
-                  <label className="" htmlFor="Thursday">
-                    Thursday
-                  </label>
-                </div>
-                <div className="d-flex align-items-center">
-                  <input className="me-2" type="checkbox" name="open_days[]" id="Friday" value="Friday" />
-                  <label className="" htmlFor="Friday">
-                    Friday
-                  </label>
-                </div>
-              </div>
-              <div className="row">
-                  <div className="col-lg-6 col-md-6 col-m-6">
-                      <label htmlFor="s-open">Shop Open</label>
-                      <input className="form-control" type="time" name="open_time" id="s-open" />
-                  </div>
-                  <div className="col-lg-6 col-md-6 col-m-6">
-                      <label htmlFor="s-close">Shop Close</label>
-                      <input className="form-control" type="time" name="close_time" id="s-close" />
-                  </div>
               </div>
               <button className="btn btn-success mt-2 common-color">Add</button>
             </form>
